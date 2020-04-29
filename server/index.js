c97a6ea79e023f7e5e07039970bcd70c4ce84809
sviatoslv <sviatoslav.boguslavskyi@gmail.com>
2020-04-29 16:13:08 +0300

    First commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3500;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Test'));
+
+app.listen(PORT, () => console.log('App listen at http://localhost:3500'));
